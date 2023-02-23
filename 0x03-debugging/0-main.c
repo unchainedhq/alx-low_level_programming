483e0ddce3e5476abe2c3c4ace453a6f4bdee2a3
unchainehq <nextgenhq691@gmail.com>
2023-02-23 12:17:17 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if positive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive or negative(i);
+
+	return (0);
+}
