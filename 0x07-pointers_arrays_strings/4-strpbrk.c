64798bf611c48767a87a58ca043373aada19faa7
unchainehq <nextgenhq691@gmail.com>
2023-03-06 03:48:22 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,15 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
+		int k;
 
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
+		while (*s)
 		{
-		if (*s == accept[k])
-		return (s);
-	s++;
-	}
+			for (k = 0; accept[k]; k++)
+			{
+			if (*s == accept[k])
+			return (s);
+			}
+		s++;
+		}
+
 	return ('\0');
 }
