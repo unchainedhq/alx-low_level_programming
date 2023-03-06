3883816e219e10d582f907b78f0a7c664dca7aed
unchainehq <nextgenhq691@gmail.com>
2023-03-06 04:22:06 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * set_string - sets the value of a point to a char
+ * set_string - sets the value of a pointer to a char
  * @s: pointer to pointer
  * @to: pointer char
  */
