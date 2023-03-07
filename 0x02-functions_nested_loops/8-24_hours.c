0edb0b6a9ae1e9a6022d2cc1048ccb97cb7300bd
unchainehq <nextgenhq691@gmail.com>
2023-03-07 14:54:22 -0800

    8-main.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
+
 /**
- * jack_baver - Print every minute of the day of jack Baver
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
+void jack_bauer(void)
+	{
+		int i, j;
 
-void jack_baver(void)
 
-{
-	int a, b, c, d;
+		i = 0;
 
-	for (a = 0; a <= 2; a++)
-	{
-	for (b = 0; b <= 9; b++)
-	{
-	if ((a <= 1 && b <= 0) || (a <= 2 && b <= 3))
-	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
+
+		while (i < 24)
+		{
+			j = 0;
+			while (j < 60)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+				j++;
+			}
+			i++;
+		}
 	}
-	}
-	}
-	}
-	}
-}
+
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    jack_brave();
+    jack_bauer();
     return (0);
 }
